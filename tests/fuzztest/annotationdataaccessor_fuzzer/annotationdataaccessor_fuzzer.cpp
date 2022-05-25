d8fd475949bc7eff330254c86ab52f1aefef7499
shawn_hu_ls <huxiaowei3@huawei.com>
2022-05-25 13:16:22 +0800

    Add fuzztest part 2
    Cherry-pick from https://gitee.com/openharmony/ark_runtime_core/pulls/144
    
    Enable runtime_core fuzz test, 10 testcases added:
    1. libpandafile
    annotationdataaccessor_fuzzer
    classdataaccessor_fuzzer
    codedataaccessor_fuzzer
    debuginfodataaccessor_fuzzer
    fielddataaccessor_fuzzer
    2. libpandabase
    load_fuzzer
    resolvesymbol_fuzzer
    begintracepoint_fuzzer
    inttracepoint_fuzzer
    int64tracepoint_fuzzer
    
    Issue: #I59NOD
    
    Test: Fuzz test on Windows, with device rk3568
    
    Signed-off-by: shawn_hu_ls <huxiaowei0506@hotmail.com>

---
+++ tests/fuzztest/annotationdataaccessor_fuzzer/annotationdataaccessor_fuzzer.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "annotationdataaccessor_fuzzer.h"
+
+#include "libpandafile/file.h"
+#include "libpandafile/annotation_data_accessor.h"
+#include "libpandafile/class_data_accessor-inl.h"
+
+namespace OHOS {
+    void AnnotationDataAccessorFuzzTest(const uint8_t* data, size_t size)
+    {
+        auto pf = panda::panda_file::OpenPandaFileFromMemory(data, size);
+        if (pf == nullptr) {
+            return;
+        }
+        auto classes = pf->GetClasses();
+        const auto &panda_file = *pf;
+        for (size_t i = 0; i < classes.Size(); i++) {
+            panda::panda_file::File::EntityId id(classes[i]);
+            if (panda_file.IsExternal(id)) {
+                continue;
+            }
+
+            panda::panda_file::ClassDataAccessor cda(panda_file, id);
+            cda.EnumerateAnnotations([&](panda::panda_file::File::EntityId id) {
+                panda::panda_file::AnnotationDataAccessor ada(panda_file, id);
+            });
+        }
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::AnnotationDataAccessorFuzzTest(data, size);
+    return 0;
+}
